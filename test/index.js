91fcb5da32f9c007228fd721da7630f964429ad6
Michael Williams <dinosaur@riseup.net>
2014-08-04 00:47:14 +0000

    add failing test for basic relation

--- test/index.js
+++ test/index.js
@@ -46,4 +46,74 @@ describe("#Graph", function () {
       expect(Graph.isGraph(graph)).to.be.true;
     });
   });
+
+  describe("relations", function () {
+
+    it("should have hasMany <> belongsTo relation", function () {
+      var types = require('oa-types')();
+      var PersonType = types.use({
+        id: "Person",
+        type: 'object',
+        properties: {
+          name: {
+            type: "string",
+          },
+          resources: {
+            reverse: "owner",
+            $ref: "Resource",
+          },
+        },
+      });
+      var ResourceType = types.use({
+        id: "Resource",
+        type: 'object',
+        properties: {
+          name: {
+            type: "string",
+          },
+          owner: {
+            $ref: "Person",
+          },
+        },
+      });
+      var People = new Graph({
+        id: "People",
+        db: db,
+        type: PersonType,
+      });
+      var Resources = new Graph({
+        id: "Resources",
+        db: db,
+        type: ResourceType,
+      });
+
+      var id;
+
+      // create person
+      return People.create({
+        name: "Mikey",
+      })
+      .then(function (person) {
+        // save person id
+        id = person['@id'];
+        // create resource with person as owner
+        return Resources.create({
+          name: "RepRap Prusa Mendel",
+          owner: id,
+        });
+      })
+      .then(function (resource) {
+        // get person again
+        return People.get(id);
+      })
+      .then(function (person) {
+        expect(person).to.have.property('resources');
+        expect(Array.isArray(person.resources)).to.be.true;
+        expect(person.resources).to.have.length(1);
+        expect(person.resources[0]).to.have.property('@id');
+        expect(person.resources[0]).to.have.property('name', "RepRap Prusa Mendel");
+        expect(person.resources[0].owner).to.deep.equal({ '@id': id });
+      });
+    });
+  });
 });
